8f3b89b15c6955dd5559e1afec59d4e27d28d93d
ytzys <ytzys@mail.ustc.edu.cn>
2015-04-21 19:42:53 +0800

    fixed the article fragment and title fragment overlaping bug

--- src/com/ytzys/fragment/ArticleFragment.java
+++ src/com/ytzys/fragment/ArticleFragment.java
@@ -1,14 +1,10 @@
 package com.ytzys.fragment;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class ArticleFragment extends Fragment {
@@ -19,8 +15,12 @@ public class ArticleFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		inflater.inflate(R.layout.article_fragment_layout, container);
-		return super.onCreateView(inflater, container, savedInstanceState);
+
+		// 注意不要用return super.onCreateView(LayoutInflater inflater, ViewGroup
+		// container, Bundle savedInstanceState);否则按下返回键时，这个Fragment依然能够显示出来
+		// 注意inflate方法的第三个参数，不加的话默认为true，会引起崩溃
+		return inflater.inflate(R.layout.article_fragment_layout, container,
+				false);
 	}
 
 	@Override
